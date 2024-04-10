26b2b8912f2ad2870710a20a5a6023a73d763ce8
David Ventura <davidventura27@gmail.com>
2024-04-10 17:50:44 +0200

    improve logic to skip parts of samples

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ mod player;
 mod wifi;
 use player::I2sPlayer;
 
+const SSID: &'static str = env!("SSID");
+const PASS: &'static str = env!("PASS");
+
 fn handle_samples<P: Player>(
     sample_rx: mpsc::Receiver<(TimeVal, Vec<u8>)>,
     time_base_c: time::Instant,
@@ -21,43 +24,47 @@ fn handle_samples<P: Player>(
 ) {
     // >= (960 * 2) for OPUS
     // >= 2880 for PCM
-    let mut samples_out = vec![0; 4096];
+    // >= 4700 for flac
+    let mut samples_out = vec![0; 4700];
 
     let mut player_lat_ms: u16 = 1;
-    //let mut hs = HeapStats_t::default();
+    let mut samples_per_ms: u16 = 1; // irrelevant, will be overwritten
+
     while let Ok((client_audible_ts, samples)) = sample_rx.recv() {
         let mut valid = true;
         let mut remaining: TimeVal;
+
+        let mut skip_samples = 0;
         loop {
             remaining = client_audible_ts - time_base_c.elapsed().into();
-            let abs = remaining.abs();
-            if remaining.sec < 0 && (abs.sec > 0 || abs.usec > 10_000) {
-                // maybe 10ms is ok? probably not
+            if remaining.sec == -1 && remaining.usec > 0 {
+                remaining.sec = 0;
+                remaining.usec -= 1_000_000;
+            }
+
+            if remaining.sec != 0 {
+                println!("rem {remaining:?} too far away! hard cutting");
                 valid = false;
                 break;
             }
 
-            // FIXME, fails often on startup
-            match remaining.millis() {
-                Ok(v) => {
-                    if v <= player_lat_ms {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    println!("Failed to call millis: {}", e);
+            // consider 0.5ms reasonable to just play
+            if remaining.usec > -500 {
+                skip_samples = 0;
+                if (remaining.usec / 1000) as u16 <= player_lat_ms {
                     break;
+                } else {
+                    std::thread::sleep(time::Duration::from_micros(499));
                 }
+            } else {
+                let ms_to_skip = (remaining.usec / 1000).abs() as u16;
+                skip_samples = ms_to_skip * samples_per_ms;
+                println!("skipping {skip_samples} samples = {ms_to_skip}ms");
+                break;
             }
-            std::thread::sleep(time::Duration::from_micros(499));
         }
 
         if !valid {
-            println!(
-                "aaa in the past {:?} - abs = {:?}",
-                remaining,
-                remaining.abs()
-            );
             continue;
         }
 
@@ -70,14 +77,18 @@ fn handle_samples<P: Player>(
         };
         // Backends with 0ms of buffer (file, tcp) otherwise behave erratically
         player_lat_ms = std::cmp::max(1, p.latency_ms().unwrap());
+        if samples_per_ms == 1 {
+            samples_per_ms = p.sample_rate() / 1000;
+        }
         let decoded_sample_c = dec.decode_sample(&samples, &mut samples_out).unwrap();
-        let sample = &samples_out[0..decoded_sample_c];
-        p.write(sample).unwrap();
+        if skip_samples as usize > decoded_sample_c {
+            println!("Tried to skip way too much, skipping the whole sample");
+            continue;
+        }
+        let sample = &mut samples_out[0..decoded_sample_c];
+        p.write(&mut sample[(skip_samples as usize)..]).unwrap();
     }
 }
-
-const SSID: &'static str = env!("SSID");
-const PASS: &'static str = env!("PASS");
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -91,18 +102,17 @@ fn main() -> anyhow::Result<()> {
     let mut peripherals = Peripherals::take().unwrap();
 
     let i2s = peripherals.i2s0;
-    let dout = peripherals.pins.gpio2;
-    let bclk = peripherals.pins.gpio4;
-    let ws = peripherals.pins.gpio15;
+    let dout = peripherals.pins.gpio19;
+    let bclk = peripherals.pins.gpio21;
+    let ws = peripherals.pins.gpio18;
     let player = I2sPlayer::new(i2s, dout, bclk, ws);
 
     let nvsp = EspDefaultNvsPartition::take().unwrap();
     wifi::configure(SSID, PASS, nvsp, &mut peripherals.modem).expect("Could not configure wifi");
 
     let client = Client::new("99:22:33:44:55:66".into(), "framework".into());
-    println!("Connecting!");
+    // TODO: discover stream
     let mut client = client.connect("192.168.2.131:1704")?;
-    println!("Connected!");
     let time_base_c = client.time_base();
 
     let dec: Arc<Mutex<Option<Decoder>>> = Arc::new(Mutex::new(None));
@@ -117,11 +127,9 @@ fn main() -> anyhow::Result<()> {
     };
     let mut local_latency = TimeVal { sec: 0, usec: 0 };
 
-    // 5.6KB buffs
-    // #  = bad/good
-    // 4  = 403/502 - 418/500
-    // 8  = 308/501
-    // 16 = 286/500
+    // Experimentally, with a queue depth of 4, 50% of the packets block for ~30ms.
+    // With a queue depth of 16, 36% of the packets block, and it requires most of the
+    // system's memory.
     let (sample_tx, sample_rx) = mpsc::sync_channel::<(TimeVal, Vec<u8>)>(4);
     std::thread::spawn(move || handle_samples(sample_rx, time_base_c, player, dec));
 
@@ -144,8 +152,9 @@ fn main() -> anyhow::Result<()> {
                 let t_s = wc.timestamp;
                 let t_c = t_s - median_tbase;
                 let audible_at = t_c + buffer_ms - local_latency;
-                // This will sometimes block on send(), to minimize memory usage (number of buffers
-                // in mem).
+                // This will sometimes block on send()
+                // to to minimize memory usage (number of buffers in mem).
+                // Effectively using the network as a buffer
                 sample_tx.send((audible_at, wc.payload.to_vec()))?;
             }
 
