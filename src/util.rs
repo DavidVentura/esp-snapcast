36add006213f13fe8ef3a33cc2c100e510b52c6f
David Ventura <davidventura27@gmail.com>
2024-04-16 22:19:57 +0200

    compare using checked duration to prevent clock sync panics

--- src/util.rs
+++ src/util.rs
@@ -3,8 +3,11 @@ use std::time::{Duration, Instant};
 pub(crate) fn measure_exec<F: FnOnce()>(name: &str, f: F, threshold: Duration) {
     let start = Instant::now();
     f();
-    let duration = start.elapsed();
-    if duration > threshold {
-        log::warn!("Calling {name} took {duration:?}");
+    let end = Instant::now();
+    let duration = end.checked_duration_since(start);
+    if let Some(duration) = duration {
+        if duration > threshold {
+            log::warn!("Calling {name} took {duration:?}");
+        }
     }
 }
